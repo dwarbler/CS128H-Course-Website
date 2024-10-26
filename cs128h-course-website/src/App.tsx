7ed2314b060dae402d334138408a157e19c8c2fc
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-10-25 23:03:56 -0500

    Feat(Hamburger): Implement hamburger nav menu

--- cs128h-course-website/src/App.tsx
+++ cs128h-course-website/src/App.tsx
@@ -1,11 +1,9 @@
-import WelcomeCard  from "./WelcomeCard.tsx"
 import Navbar from "./Navbar.tsx"
 
 function App() {
   return (
     <>
       <Navbar />
-      <WelcomeCard />
     </>
   )
 }
--- cs128h-course-website/src/Navbar.tsx
+++ cs128h-course-website/src/Navbar.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react"
+
 export default function Navbar(): JSX.Element {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    }
+    
     return (
         <nav className="navBar">
-            <a href="">Home</a>
-            <a href="">Course Info</a>
-            <a href="">Lectures</a>
-            <a href="">Grades</a>
-            <a href="">Staff</a>
+            <div className={`links ${isOpen ? "active" : ""}`}>
+                <a href="">Home</a>
+                <a href="">Course Info</a>
+                <a href="">Lectures</a>
+                <a href="">Grades</a>
+                <a href="">Staff</a>
+            </div>
+            <div className="burger" onClick={toggleMenu}>☰</div>
         </nav>
     )
 }
