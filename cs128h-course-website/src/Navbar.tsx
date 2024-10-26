10d737b9f1c4e2a5cd74f0cb9912ef8663927c38
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-10-26 15:30:18 -0500

    Refactor(Navbar): Start Navbar/Homepage rewrite

--- cs128h-course-website/src/Navbar.tsx
+++ cs128h-course-website/src/Navbar.tsx
@@ -1,26 +1,14 @@
-import { useState } from "react"
 import courseLogo from "./assets/logo.png"
 
 export default function Navbar(): JSX.Element {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    }
-    
     return (
         <nav className="navBar">
             <img className="logo" src={courseLogo} alt="CS128H Logo"></img>
-            <div className="menuContainer">
-                <div className={`links ${isOpen ? "active" : ""}`}>
-                    <a href="">Home</a>
-                    <a href="">Course Info</a>
-                    <a href="">Lectures</a>
-                    <a href="">Grades</a>
-                    <a href="">Staff</a>
-                </div>
-                <div className="burger" onClick={toggleMenu}>☰</div>
-            </div>
+            <a href="">Home</a>
+            <a href="">Course Info</a>
+            <a href="">Lectures</a>
+            <a href="">Grades</a>
+            <a href="">Staff</a>
         </nav>
     )
 }
