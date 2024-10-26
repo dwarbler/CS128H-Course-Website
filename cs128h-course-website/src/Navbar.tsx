bf8df3962a4be7ee0f5c366c46805fef7f196552
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-10-25 23:19:13 -0500

    Fix(Navbar): Add logo to navbar

--- cs128h-course-website/src/Navbar.tsx
+++ cs128h-course-website/src/Navbar.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import courseLogo from "./assets/logo.png"
 
 export default function Navbar(): JSX.Element {
     const [isOpen, setIsOpen] = useState(false);
@@ -9,14 +10,17 @@ export default function Navbar(): JSX.Element {
     
     return (
         <nav className="navBar">
-            <div className={`links ${isOpen ? "active" : ""}`}>
-                <a href="">Home</a>
-                <a href="">Course Info</a>
-                <a href="">Lectures</a>
-                <a href="">Grades</a>
-                <a href="">Staff</a>
+            <img className="logo" src={courseLogo} alt="CS128H Logo"></img>
+            <div className="menuContainer">
+                <div className={`links ${isOpen ? "active" : ""}`}>
+                    <a href="">Home</a>
+                    <a href="">Course Info</a>
+                    <a href="">Lectures</a>
+                    <a href="">Grades</a>
+                    <a href="">Staff</a>
+                </div>
+                <div className="burger" onClick={toggleMenu}>☰</div>
             </div>
-            <div className="burger" onClick={toggleMenu}>☰</div>
         </nav>
     )
 }
