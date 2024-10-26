cd779ab3bb2cb142d207e065ef72799abdbfa5eb
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-10-26 15:40:35 -0500

    Refactor(Navbar): Re-style navbar element

--- cs128h-course-website/src/Navbar.tsx
+++ cs128h-course-website/src/Navbar.tsx
@@ -4,11 +4,13 @@ export default function Navbar(): JSX.Element {
     return (
         <nav className="navBar">
             <img className="logo" src={courseLogo} alt="CS128H Logo"></img>
-            <a href="">Home</a>
-            <a href="">Course Info</a>
-            <a href="">Lectures</a>
-            <a href="">Grades</a>
-            <a href="">Staff</a>
+            <div className="links">
+                <a href="">Home</a>
+                <a href="">Course Info</a>
+                <a href="">Lectures</a>
+                <a href="">Grades</a>
+                <a href="">Staff</a>
+            </div>
         </nav>
     )
 }
