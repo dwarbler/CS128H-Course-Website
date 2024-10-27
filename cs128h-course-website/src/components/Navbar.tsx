c304aacf676d68fa226f6a3d7640a8d2d86ad51e
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-10-26 21:20:53 -0500

    Feat(Router): Use Link component instead of <a>

--- cs128h-course-website/src/components/Navbar.tsx
+++ cs128h-course-website/src/components/Navbar.tsx
@@ -1,15 +1,16 @@
 import courseLogo from "../assets/logo.png"
+import { Link } from "react-router-dom"
 
 export default function Navbar(): JSX.Element {
     return (
         <nav className="navBar">
             <img className="logo" src={courseLogo} alt="CS128H Logo"></img>
             <div className="links">
-                <a href="">Home</a>
-                <a href="">Course Info</a>
-                <a href="">Lectures</a>
-                <a href="">Grades</a>
-                <a href="">Staff</a>
+                <Link to="/">Home</Link>
+                <Link to="/CourseInfo">Course Info</Link>
+                <Link to="/Lectures">Lectures</Link>
+                <Link to="/Grades">Grades</Link>
+                <Link to="/Staff">Staff</Link>
             </div>
         </nav>
     )
