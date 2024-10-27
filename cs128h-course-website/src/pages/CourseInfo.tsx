22b0126c236c4c96b20781ea231e5c48e2882322
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-10-27 15:13:02 -0500

    Doc(Course Info): Add docstring to course info page

--- cs128h-course-website/src/pages/CourseInfo.tsx
+++ cs128h-course-website/src/pages/CourseInfo.tsx
@@ -5,6 +5,18 @@ import Policies from "../components/Policies"
 import Usefullinks from "../components/Usefullinks"
 
 export default function CourseInfo(): JSX.Element {
+    /**
+     * Returns the JSX Element corresponding to the Course Info page
+     * 
+     * Utilizes components as props for generalization of the
+     * Collapsible component.
+     * 
+     * Add New Section - Create a new component which returns the JSX
+     * to be inside the new section and add new collapsible component
+     * below.
+     * 
+     * @returns The Webpage JSX
+     */
     return (
         <>
             <Navbar />
