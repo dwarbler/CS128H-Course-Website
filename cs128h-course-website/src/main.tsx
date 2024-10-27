e569faa2b09d5e9f810ba32512d3f708455ab86a
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-10-26 21:17:09 -0500

    Feat(Router): Add BrowserRouter for routing

--- cs128h-course-website/src/main.tsx
+++ cs128h-course-website/src/main.tsx
@@ -1,12 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Navbar from "./components/Navbar.tsx"
-import Hero from "./components/Hero.tsx"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import CourseInfo from './pages/CourseInfo.tsx'
+import Lectures from './pages/Lectures'
+import Grades from './pages/Grades'
+import Staff from './pages/Staff'
+import NoPage from './pages/NoPage.tsx'
+
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Navbar />
-    <Hero />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/CourseInfo" element={<CourseInfo />} />
+        <Route path="/Lectures" element={<Lectures />} />
+        <Route path="/Grades" element={<Grades />} />
+        <Route path="/Staff" element={<Staff />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ cs128h-course-website/src/pages/NoPage.tsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar"
+
+export default function NoPage(): JSX.Element {
+    return (
+        <>
+            <Navbar />
+            <p>Oops!</p>
+        </>
+    )
+}
