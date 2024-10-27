9706feb8c77b7c99b21c1b4f8cb547bc73f282d1
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-10-27 15:16:32 -0500

    Doc(Navbar): Added docstring to navbar component

--- cs128h-course-website/src/components/Navbar.tsx
+++ cs128h-course-website/src/components/Navbar.tsx
@@ -2,6 +2,14 @@ import courseLogo from "../assets/logo.png"
 import { Link } from "react-router-dom"
 
 export default function Navbar(): JSX.Element {
+    /**
+     * Returns the JSX corresponding to the navbar at the top
+     * of the site.
+     * 
+     * This utilizes the React BrowserRouter setup in main.tsx
+     * 
+     * @returns JSX With React Router Links to site subpages
+     */
     return (
         <nav className="navBar">
             <img className="logo" src={courseLogo} alt="CS128H Logo"></img>
