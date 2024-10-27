53b7867ac5cd0f0fd2045b43b0b2de4a92d6ab6e
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-10-26 21:10:23 -0500

    Chore(Router): Add page components

---
+++ cs128h-course-website/src/pages/CourseInfo.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar"
+import Hero from "../components/Hero"
+
+export default function CourseInfo(): JSX.Element {
+    return (
+        <>
+            <Navbar />
+            <Hero />
+        </>
+    )
+}
---
+++ cs128h-course-website/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar"
+import Hero from "../components/Hero"
+
+export default function Home(): JSX.Element {
+    return (
+        <>
+            <Navbar />
+            <Hero />
+        </>
+    )
+}
