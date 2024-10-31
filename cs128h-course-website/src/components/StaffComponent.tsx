090058c51118fbc582337cf08eb26e3fb5b890f9
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-10-30 21:58:33 -0500

    Style(Staff): Add CSS styling to the staff page

--- cs128h-course-website/src/components/StaffComponent.tsx
+++ cs128h-course-website/src/components/StaffComponent.tsx
@@ -1,5 +1,3 @@
-import Collapsible from "./Collapsible"
-
 import data from "../assets/staff/staff.json"
 
 export default function StaffComponent(): JSX.Element {
@@ -11,11 +9,8 @@ export default function StaffComponent(): JSX.Element {
                     data.map(staff => (
                         <>
                             <div className="staff">
-                                <img key={staff.id} src={`/src/assets/staff/${staff.image}`} alt="Derek Staff Image"/>
-                                <Collapsible
-                                    title={`${staff.name}`}
-                                    BodyComponent={<p>${staff.description}</p>}
-                                />
+                                <img key={staff.id} src={`/src/assets/staff/${staff.image}`} alt="Derek Staff Image" className="staffPicture" />
+                                <p>{staff.description}</p>
                             </div>
                         </>
                     ))
--- cs128h-course-website/src/pages/Staff.tsx
+++ cs128h-course-website/src/pages/Staff.tsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar"
+import StaffComponent from "../components/StaffComponent"
 
 export default function Staff(): JSX.Element {
     return (
         <>
             <Navbar />
+            <StaffComponent />
         </>
     )
 }
