269ce077da327003cf24ef24ee2dab824869e2d6
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-12-03 00:26:00 -0500

    Chore(setup): Work on some IDE setup

--- cs128h-course-website/src/pages/Lectures.tsx
+++ cs128h-course-website/src/pages/Lectures.tsx
@@ -2,10 +2,10 @@ import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
 
 export default function Lectures(): JSX.Element {
-    return (
-        <>
-            <Navbar />
-            <Hero />
-        </>
-    )
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  )
 }
