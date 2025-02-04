cb1b7e2affe232d32c097dd18ea7f527cd88abe0
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2025-02-04 12:01:16 -0600

    Feat(Page): Implement variable Page component with header and body

---
+++ cs128h-course-website/src/pages/Page.tsx
@@ -0,0 +1,39 @@
+import { AppSidebar } from "@/components/Sidebar";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+    SidebarInset,
+    SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+export default function Page(title: string, body: JSX.Element) {
+    return (
+        <>
+            <AppSidebar />
+            <SidebarInset>
+                <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                    <SidebarTrigger className="-m1-1" />
+                    <Separator orientation="vertical" className="mr-2 h-4" />
+                    <Breadcrumb>
+                        <BreadcrumbList>
+                            <BreadcrumbItem className="hidden md:block">
+                                <BreadcrumbPage>Home</BreadcrumbPage>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator className="hidden md:block" />
+                            <BreadcrumbItem>
+                                <BreadcrumbPage>{title}</BreadcrumbPage>
+                            </BreadcrumbItem>
+                        </BreadcrumbList>
+                    </Breadcrumb>
+                </header>
+                {body}
+            </SidebarInset>
+        </>
+    )
+}
