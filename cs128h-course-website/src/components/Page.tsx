9c27fa7e945c8dad77d4781b1781aeb32e0729b9
dwarbler <92753567+dwarbler@users.noreply.github.com>
2025-04-26 17:44:57 -0500

    style: fix theme switch placement

--- cs128h-course-website/src/components/Page.tsx
+++ cs128h-course-website/src/components/Page.tsx
@@ -18,22 +18,24 @@ export function Page({ ...props }): JSX.Element {
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-m1-1" />
           <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbPage>Home</BreadcrumbPage>
-              </BreadcrumbItem>
-              {props.breadcrumb ? (
-                <>
-                  <BreadcrumbSeparator className="hidden md:block" />
-                  <BreadcrumbItem>
-                    <BreadcrumbPage>{props.title}</BreadcrumbPage>
-                  </BreadcrumbItem>
-                </>
-              ) : null}
-            </BreadcrumbList>
-          </Breadcrumb>
-          <ModeToggle />
+          <div className="flex justify-between items-center size-full">
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbPage>Home</BreadcrumbPage>
+                </BreadcrumbItem>
+                {props.breadcrumb ? (
+                  <>
+                    <BreadcrumbSeparator className="hidden md:block" />
+                    <BreadcrumbItem>
+                      <BreadcrumbPage>{props.title}</BreadcrumbPage>
+                    </BreadcrumbItem>
+                  </>
+                ) : null}
+              </BreadcrumbList>
+            </Breadcrumb>
+            <ModeToggle />
+          </div>
         </header>
         {props.body}
       </SidebarInset>
