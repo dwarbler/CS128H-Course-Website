9f61b1ac20a83d22dafafd7e04e6736edb812b80
dwarbler <92753567+dwarbler@users.noreply.github.com>
2025-04-26 17:31:02 -0500

    feat: add first iteration of theme switcher

--- cs128h-course-website/src/components/Page.tsx
+++ cs128h-course-website/src/components/Page.tsx
@@ -1,41 +1,42 @@
 import { AppSidebar } from "@/components/Sidebar";
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator"
-import {
-    SidebarInset,
-    SidebarTrigger,
-} from "@/components/ui/sidebar"
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
+import { ModeToggle } from "./mode-toggle";
 
 export function Page({ ...props }): JSX.Element {
-    return (
-        <>
-            <AppSidebar />
-            <SidebarInset>
-                <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-                    <SidebarTrigger className="-m1-1" />
-                    <Separator orientation="vertical" className="mr-2 h-4" />
-                    <Breadcrumb>
-                        <BreadcrumbList>
-                            <BreadcrumbItem className="hidden md:block">
-                                <BreadcrumbPage>Home</BreadcrumbPage>
-                            </BreadcrumbItem>
-                            {(props.breadcrumb) ? (<>
-                                <BreadcrumbSeparator className="hidden md:block" />
-                                <BreadcrumbItem>
-                                    <BreadcrumbPage>{props.title}</BreadcrumbPage>
-                                </BreadcrumbItem>
-                            </>) : null}
-                        </BreadcrumbList>
-                    </Breadcrumb>
-                </header>
-                {props.body}
-            </SidebarInset>
-        </>
-    )
+  return (
+    <>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-m1-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbPage>Home</BreadcrumbPage>
+              </BreadcrumbItem>
+              {props.breadcrumb ? (
+                <>
+                  <BreadcrumbSeparator className="hidden md:block" />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>{props.title}</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </>
+              ) : null}
+            </BreadcrumbList>
+          </Breadcrumb>
+          <ModeToggle />
+        </header>
+        {props.body}
+      </SidebarInset>
+    </>
+  );
 }
